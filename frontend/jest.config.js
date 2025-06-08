fc5f105f7ebd73f13355c32e0e21d21393bc2d49
Sushi <abdallaelshafei@gmail.com>
2025-06-08 03:03:38 -0300

    Add CI improvements and scheduled DB generation

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
+  },
 };
